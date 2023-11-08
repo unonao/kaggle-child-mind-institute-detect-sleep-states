f5ed3369ccb98d48cee108995a65402c22eb4ed7
unonao <naokiuno634@gmail.com>
2023-11-08 22:30:02 +0000

    閾値調整のcfgを微修正

--- run/search.py
+++ run/search.py
@@ -25,9 +25,9 @@ def main(cfg: DictConfig):  # type: ignore
     """
     # print(cfg)
 
-    event_df = pl.read_csv(Path(cfg.train.dir.data_dir) / "train_events.csv").drop_nulls()
-    val_pred_df = event_df.filter(pl.col("series_id").is_in(cfg.train.split.valid_series_ids))
-    distance = cfg.train.post_process.distance
+    event_df = pl.read_csv(Path(cfg.dir.data_dir) / "train_events.csv").drop_nulls()
+    val_pred_df = event_df.filter(pl.col("series_id").is_in(cfg.split.valid_series_ids))
+    distance = cfg.post_process.distance
 
     #
     exp_dir = Path(os.path.join(cfg.base_dir, cfg.exp, "single"))
@@ -37,22 +37,21 @@ def main(cfg: DictConfig):  # type: ignore
     preds = np.load(exp_dir / "preds.npy")
 
     # search th
-    """
-    score, th = score_ternary_search_th(
-        val_event_df=val_pred_df,
-        keys=keys,
-        preds=preds,
-        distance=distance,
-    )
-    """
-
-    # search distance
-    score, th = score_ternary_search_distance(
-        val_event_df=val_pred_df,
-        keys=keys,
-        preds=preds,
-        score_th=cfg.train.post_process.score_th,
-    )
+    if cfg.how == "threshold":
+        score, th = score_ternary_search_th(
+            val_event_df=val_pred_df,
+            keys=keys,
+            preds=preds,
+            distance=distance,
+        )
+    elif cfg.how == "distance":
+        # search distance
+        score, th = score_ternary_search_distance(
+            val_event_df=val_pred_df,
+            keys=keys,
+            preds=preds,
+            score_th=cfg.post_process.score_th,
+        )
 
     LOGGER.info(f"score: {score}, th: {th}")
 
