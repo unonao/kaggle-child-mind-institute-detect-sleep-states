6e61879cbfb8fb9ca67f5587c7078b7969f26a7b
unonao <naokiuno634@gmail.com>
2023-11-15 14:49:05 +0000

    引数のバグ修正

--- src/models/loss/bce.py
+++ src/models/loss/bce.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 
 class BCEWithLogitsLoss(nn.Module):
-    def __init__(self, label_weight: torch.Tensor = None, pos_weight: torch.Tensor = None):
+    def __init__(self, weight: torch.Tensor = None, pos_weight: torch.Tensor = None):
         super(BCEWithLogitsLoss, self).__init__()
-        self.label_weight = label_weight
+        self.weight = weight
         self.pos_weight = pos_weight
-        self.loss_fn = nn.BCEWithLogitsLoss(weight=self.label_weight, pos_weight=self.pos_weight)
+        self.loss_fn = nn.BCEWithLogitsLoss(weight=self.weight, pos_weight=self.pos_weight)
 
     def forward(self, logits, labels, masks):
         loss = self.loss_fn(logits, labels)
