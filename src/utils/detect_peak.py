de96979d96f0c6f610e37bfb0c4efe2d0df1da2d
unonao <naokiuno634@gmail.com>
2023-12-02 13:20:25 +0000

    numbaによる高速化

--- src/utils/detect_peak.py
+++ src/utils/detect_peak.py
@@ -1,6 +1,7 @@
 import numpy as np
 import polars as pl
 from tqdm.auto import tqdm
+from numba import jit
 
 
 def post_process_from_2nd(
@@ -14,8 +15,6 @@ def post_process_from_2nd(
 ):
     """
     1分ごとの予測値を用いてイベントを検出する
-    # TODO: 1段目のモデルを入れる場合は1分ごとの予測値をそのまま使わずに周辺の予測値をrollする方が良さそう？
-
     用語
     - 予測地点: 2段目のモデルによって得られた1分毎の予測位置
     - 候補地点: event の候補となる 15秒 or 45秒始まりの30秒間隔の位置
@@ -31,12 +30,9 @@ def post_process_from_2nd(
     Returns:
         event_df (pl.DataFrame): row_id, series_id, step, event, score をカラムに持つ。
     """
-    high_match_nums = [1, 3, 5, 8, 10, 13, 15, 20, 25, 30]
-    low_match_nums = [1, 3, 5, 7, 10, 12, 15, 20, 25, 30]
-    match_sums = [np.power(weight_rate, i) for i in range(10)] if weight_rate else np.ones(10)
-    # match_sums = [1.0+weight_rate*i for i in range(10)] if weight_rate else np.ones(10)
-    total_num = sum(high_match_nums + low_match_nums)
-    # total_num = sum(high_match_nums + low_match_nums)
+    high_match_nums = (0, 1, 3, 5, 8, 10, 13, 15, 20, 25, 30)
+    low_match_nums = (0, 1, 3, 5, 7, 10, 12, 15, 20, 25, 30)
+    match_sums = np.array([np.power(weight_rate, i) for i in range(10)]) if weight_rate else np.ones(10)
     result_events_records = []
 
     # event ごとに処理
@@ -85,7 +81,7 @@ def post_process_from_2nd(
                         .over(["series_id", "chunk_id"])
                         / match_sums[i]
                     )
-                    for i, window in enumerate(high_match_nums)
+                    for i, window in enumerate(high_match_nums[1:])
                 ]
             ).alias(f"{event}_left_expectation_plus_3step"),
             pl.sum_horizontal(
@@ -96,7 +92,7 @@ def post_process_from_2nd(
                         .over(["series_id", "chunk_id"])
                         / match_sums[i]
                     )
-                    for i, window in enumerate(low_match_nums)
+                    for i, window in enumerate(low_match_nums[1:])
                 ]
             ).alias(f"{event}_left_expectation_plus_9step"),
         )
@@ -114,7 +110,7 @@ def post_process_from_2nd(
                         .fill_null(0)
                         / match_sums[i]
                     )
-                    for i, window in enumerate(low_match_nums)
+                    for i, window in enumerate(low_match_nums[1:])
                 ]
             ).alias(f"{event}_right_expectation_plus_3step"),
             pl.sum_horizontal(
@@ -127,7 +123,7 @@ def post_process_from_2nd(
                         .fill_null(0)
                         / match_sums[i]
                     )
-                    for i, window in enumerate(high_match_nums)
+                    for i, window in enumerate(high_match_nums[1:])
                 ]
             ).alias(f"{event}_right_expectation_plus_9step"),
         )
@@ -168,7 +164,7 @@ def post_process_from_2nd(
                     f"{event}_expectation_sum_9step",
                 ]
             ).group_by("series_id"),
-            desc="find peaks",
+            desc=f"detect {event} peaks",
             leave=False,
             total=len(minute_pred_df["series_id"].unique()),
         ):
@@ -182,149 +178,36 @@ def post_process_from_2nd(
             expectation_sum_3step = series_df[f"{event}_expectation_sum_3step"].to_numpy(writable=True)
             expectation_sum_9step = series_df[f"{event}_expectation_sum_9step"].to_numpy(writable=True)
             steps = series_df[f"step"].to_numpy(writable=True)
-            step_id_mins = series_df["min_id_in_chunk"].to_numpy()
-            step_id_maxs = series_df["max_id_in_chunk"].to_numpy() + 1
+            step_id_mins = series_df["min_id_in_chunk"].to_numpy(writable=True)
+            step_id_maxs = series_df["max_id_in_chunk"].to_numpy(writable=True) + 1
             powers = np.ones(len(expectation_sum_3step), dtype=np.int32) * 10
-            for _ in range(max_event_per_series):  # 高い順に最大max_event_per_series個のeventを決定
-                # 3.1 最大値の取得
-                # 合計の期待値が最大のstepを取得
-                max_step3 = expectation_sum_3step.argmax()
-                max_score3 = expectation_sum_3step[max_step3]
-                max_step9 = expectation_sum_9step.argmax()
-                max_score9 = expectation_sum_9step[max_step9]
-                if max_score3 > max_score9:
-                    # print('max_score3')
-                    left_nums = [0] + high_match_nums
-                    right_nums = [0] + low_match_nums
-                    max_step_index = max_step3
-                    max_score = max_score3
-                    result_events_records.append(
-                        {
-                            "series_id": series_id,
-                            "step": steps[max_step_index] + 3,
-                            "event": event,
-                            "score": max_score,
-                        }
-                    )
-                else:
-                    # print('max_score9')
-                    left_nums = [0] + low_match_nums
-                    right_nums = [0] + high_match_nums
-                    max_step_index = max_step9
-                    max_score = max_score9
-                    result_events_records.append(
-                        {
-                            "series_id": series_id,
-                            "step": steps[max_step_index] + 9,
-                            "event": event,
-                            "score": max_score,
-                        }
-                    )
-                if max_score < height:  # 閾値以下なら終了
-                    break
-                # print(f"max_step_index:{max_step_index}, max_score:{max_score}")
 
-                # 3.2 期待値の割引
-                """
-                各予測地点のpowerを修正するとともに、候補地点の期待値を割引く。
-                powerが pi まで小さくなることによってその予測値が影響を与える範囲が狭くなる。
-                つまり狭くなって範囲から抜けた expectation_sum の値が、その予測値の値*重みの分だけ小さくなる
-                """
-                # 3.2.1 まずはpowerを修正するstepの候補を探す
-                target_step_powers = []  # (target_step, pred, base_power, power, step_min, step_max)のリスト
-                for pi in range(0, 10):
-                    # 左側
-                    for l_diff in range(left_nums[pi], left_nums[pi + 1]):
-                        target_step_index = max_step_index - l_diff
-                        if target_step_index < 0:
-                            break
-                        pred = preds[target_step_index]
-                        base_power = powers[target_step_index]
-                        if base_power > pi:  # power が小さくなる場合のみ修正
-                            target_step_powers.append(
-                                (
-                                    target_step_index,
-                                    pred,
-                                    base_power,
-                                    pi,
-                                    step_id_mins[target_step_index],
-                                    step_id_maxs[target_step_index],
-                                )
-                            )
-                    # 右側
-                    for r_diff in range(right_nums[pi] + 1, right_nums[pi + 1] + 1):  # 自分自身と同じindexは含めない
-                        target_step_index = max_step_index + r_diff
-                        if target_step_index >= len(powers):
-                            break
-                        pred = preds[target_step_index]
-                        base_power = powers[target_step_index]
-                        if base_power > pi:
-                            target_step_powers.append(
-                                (
-                                    target_step_index,
-                                    pred,
-                                    base_power,
-                                    pi,
-                                    step_id_mins[target_step_index],
-                                    step_id_maxs[target_step_index],
-                                )
-                            )
-                # print('target_step_powers', target_step_powers)
+            result_steps, result_scores = detect_events_for_serie(
+                height,
+                max_event_per_series,
+                high_match_nums,
+                low_match_nums,
+                match_sums,
+                steps,
+                step_id_mins,
+                step_id_maxs,
+                preds,
+                expectation_sum_3step,
+                expectation_sum_9step,
+                powers,
+            )
 
-                # 3.2.2 対象となる step の power を修正するとともに期待値を割り引く
-                """
-                予測地点のpowerを下げるとともに、関連する候補地点の期待値を修正する。
-                検出したeventから遠い予測地点の場合は、予測地点に近い候補地点であっても期待値はその分割り引かれる。
-                3stepの修正をする時は target_stepから左側が low_match_nums, 右側が high_match_nums
-                9stepの修正をする時は target_stepから左側が high_match_nums, 右側が low_match_nums
-                だんだんと内側のみがのこるように修正する。
+            for i in range(len(result_steps)):
+                result_events_records.append(
+                    {
+                        "series_id": series_id,
+                        "step": result_steps[i],
+                        "event": event,
+                        "score": result_scores[i],
+                    }
+                )
 
-                - powerが 10 → 8 になるケースは左右1~30個に影響を及ぼしていたものが、左右の1~20個に影響を及ぼすようになる。また、powerが2個減った分全体の期待値も割り引かれる
-                - powerが 10 → 5 になるケースは左右1~30個に影響を及ぼしていたものが、左右の1~12(13)個に影響を及ぼすようになる
-                - powerが 8 → 7 になるケースは左右1~20個に影響を及ぼしていたものが、左右の1~15個に影響を及ぼすようになる
-                """
-                # print(expectation_sum_3step[max_step_index], expectation_sum_9step[max_step_index])
-                for si, pred, base_power, power, step_min, step_max in target_step_powers:
-                    # print(f"si:{si}, pred:{pred}, base_power:{base_power}, power:{power}")
-                    powers[si] = power
-                    # 中心ほど重みが強いので power ごとに処理
-                    for pi in range(
-                        base_power, power, -1
-                    ):  # base_powerからpowerに減らしていくことで予測値の外側から削る
-                        # 3step
-                        left_nums = [0] + low_match_nums
-                        right_nums = [0] + high_match_nums
-                        left_diff_max = left_nums[pi]
-                        right_diff_max = right_nums[pi]
-                        expectation_sum_3step[
-                            max(si - left_diff_max, step_min) : min(si + right_diff_max, step_max)
-                        ] -= pred / match_sums[pi - 1]
-                        """
-                        if ((si-left_diff_max <= max_step_index) and (max_step_index < si+right_diff_max)):
-                            print(f'3step pi:{pi}')
-                            print(f"max_step_index:{max_step_index}, si:{si}, left_diff_max:{left_diff_max}, right_diff_max:{right_diff_max}") 
-                            print("[", si-left_diff_max, si+right_diff_max, ")")
-                            print(f"power: {pi}→{pi-1}, pred:{pred}")
-                            print()
-                        """
-                        # 9step
-                        left_nums = [0] + high_match_nums
-                        right_nums = [0] + low_match_nums
-                        left_diff_max = left_nums[pi]
-                        right_diff_max = right_nums[pi]
-                        expectation_sum_9step[
-                            max(si - left_diff_max, step_min) : min(si + right_diff_max, step_max)
-                        ] -= pred / match_sums[pi - 1]
-                        """
-                        if ((si-left_diff_max <= max_step_index) and (max_step_index < si+right_diff_max)):
-                            print(f'9step pi:{pi}')
-                            print(f"max_step_index:{max_step_index}, si:{si}, left_diff_max:{left_diff_max}, right_diff_max:{right_diff_max}") 
-                            print("[", si-left_diff_max, si+right_diff_max, ")")
-                            print(f"power: {pi}→{pi-1}, pred:{pred}")
-                            print()
-                        """
-
-                # print(expectation_sum_3step[max_step_index], expectation_sum_9step[max_step_index])
+            # print(expectation_sum_3step[max_step_index], expectation_sum_9step[max_step_index])
 
     if len(result_events_records) == 0:  # 一つも予測がない場合はdummyを入れる
         result_events_records.append(
@@ -339,6 +222,145 @@ def post_process_from_2nd(
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
     return sub_df
+
+
+@jit(nopython=True, cache=True)
+def detect_events_for_serie(
+    height,
+    max_event_per_series,
+    high_match_nums,
+    low_match_nums,
+    match_sums,
+    steps,
+    step_id_mins,
+    step_id_maxs,
+    preds,
+    expectation_sum_3step,
+    expectation_sum_9step,
+    powers,
+):
+    result_steps = []
+    result_scores = []
+    for _ in range(max_event_per_series):  # 高い順に最大max_event_per_series個のeventを決定
+        # 3.1 最大値の取得
+        # 合計の期待値が最大のstepを取得
+        max_step3 = expectation_sum_3step.argmax()
+        max_score3 = expectation_sum_3step[max_step3]
+        max_step9 = expectation_sum_9step.argmax()
+        max_score9 = expectation_sum_9step[max_step9]
+        if max_score3 > max_score9:
+            # print('max_score3')
+            left_nums = high_match_nums
+            right_nums = low_match_nums
+            max_step_index = max_step3
+            max_score = max_score3
+            result_steps.append(steps[max_step_index] + 3)
+            result_scores.append(max_score)
+        else:
+            # print('max_score9')
+            left_nums = low_match_nums
+            right_nums = high_match_nums
+            max_step_index = max_step9
+            max_score = max_score9
+            result_steps.append(steps[max_step_index] + 9)
+            result_scores.append(max_score)
+        if max_score < height:  # 閾値以下なら終了
+            break
+        # print(f"max_step_index:{max_step_index}, max_score:{max_score}")
+
+        # 3.2 期待値の割引
+        """
+        各予測地点のpowerを修正するとともに、候補地点の期待値を割引く。
+        powerが pi まで小さくなることによってその予測値が影響を与える範囲が狭くなる。
+        つまり狭くなって範囲から抜けた expectation_sum の値が、その予測値の値*重みの分だけ小さくなる
+        """
+        # 3.2.1 まずはpowerを修正するstepの候補を探す
+        target_step_powers = []  # (target_step, pred, base_power, power, step_min, step_max)のリスト
+        for pi in range(0, 10):
+            # 左側
+            for l_diff in range(left_nums[pi], left_nums[pi + 1]):
+                target_step_index = max_step_index - l_diff
+                if target_step_index < 0:
+                    break
+                pred = preds[target_step_index]
+                base_power = powers[target_step_index]
+                if base_power > pi:  # power が小さくなる場合のみ修正
+                    target_step_powers.append(
+                        (
+                            target_step_index,
+                            pred,
+                            base_power,
+                            pi,
+                            step_id_mins[target_step_index],
+                            step_id_maxs[target_step_index],
+                        )
+                    )
+            # 右側
+            for r_diff in range(right_nums[pi] + 1, right_nums[pi + 1] + 1):  # 自分自身と同じindexは含めない
+                target_step_index = max_step_index + r_diff
+                if target_step_index >= len(powers):
+                    break
+                pred = preds[target_step_index]
+                base_power = powers[target_step_index]
+                if base_power > pi:
+                    target_step_powers.append(
+                        (
+                            target_step_index,
+                            pred,
+                            base_power,
+                            pi,
+                            step_id_mins[target_step_index],
+                            step_id_maxs[target_step_index],
+                        )
+                    )
+        # print('target_step_powers', target_step_powers)
+
+        # 3.2.2 対象となる step の power を修正するとともに期待値を割り引く
+        """
+        予測地点のpowerを下げるとともに、関連する候補地点の期待値を修正する。
+        検出したeventから遠い予測地点の場合は、予測地点に近い候補地点であっても期待値はその分割り引かれる。
+        3stepの修正をする時は target_stepから左側が low_match_nums, 右側が high_match_nums
+        9stepの修正をする時は target_stepから左側が high_match_nums, 右側が low_match_nums
+        だんだんと内側のみがのこるように修正する。
+
+        - powerが 10 → 8 になるケースは左右1~30個に影響を及ぼしていたものが、左右の1~20個に影響を及ぼすようになる。また、powerが2個減った分全体の期待値も割り引かれる
+        - powerが 10 → 5 になるケースは左右1~30個に影響を及ぼしていたものが、左右の1~12(13)個に影響を及ぼすようになる
+        - powerが 8 → 7 になるケースは左右1~20個に影響を及ぼしていたものが、左右の1~15個に影響を及ぼすようになる
+        """
+        for si, pred, base_power, power, step_min, step_max in target_step_powers:
+            # print(f"si:{si}, pred:{pred}, base_power:{base_power}, power:{power}")
+            powers[si] = power
+            # 中心ほど重みが強いので power ごとに処理
+            for pi in range(base_power, power, -1):  # base_powerからpowerに減らしていくことで予測値の外側から削る
+                # 3step
+                left_diff_max = low_match_nums[pi]
+                right_diff_max = high_match_nums[pi]
+                expectation_sum_3step[max(si - left_diff_max, step_min) : min(si + right_diff_max, step_max)] -= (
+                    pred / match_sums[pi - 1]
+                )
+                """
+                if ((si-left_diff_max <= max_step_index) and (max_step_index < si+right_diff_max)):
+                    print(f'3step pi:{pi}')
+                    print(f"max_step_index:{max_step_index}, si:{si}, left_diff_max:{left_diff_max}, right_diff_max:{right_diff_max}") 
+                    print("[", si-left_diff_max, si+right_diff_max, ")")
+                    print(f"power: {pi}→{pi-1}, pred:{pred}")
+                    print()
+                """
+                # 9step
+                left_diff_max = high_match_nums[pi]
+                right_diff_max = low_match_nums[pi]
+                expectation_sum_9step[max(si - left_diff_max, step_min) : min(si + right_diff_max, step_max)] -= (
+                    pred / match_sums[pi - 1]
+                )
+                """
+                if ((si-left_diff_max <= max_step_index) and (max_step_index < si+right_diff_max)):
+                    print(f'9step pi:{pi}')
+                    print(f"max_step_index:{max_step_index}, si:{si}, left_diff_max:{left_diff_max}, right_diff_max:{right_diff_max}") 
+                    print("[", si-left_diff_max, si+right_diff_max, ")")
+                    print(f"power: {pi}→{pi-1}, pred:{pred}")
+                    print()
+                """
+    return result_steps, result_scores
 
 
 def remove_periodicity(
